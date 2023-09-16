5d0be1a73fab962d1744cef29cecf7a566c53d78
Tyler <5478568+Tyler-Yates@users.noreply.github.com>
2023-09-16 13:06:43 -0500

    Add cache resetting to the cronjob

--- pricehistory/__main__.py
+++ pricehistory/__main__.py
@@ -1,5 +1,7 @@
 import json
+import logging
 
+import redis
 import requests
 
 from pricehistory.constants import DB_CONNECTION_STRING
@@ -7,6 +9,13 @@ from pricehistory.cookie_util import get_cookies
 from pricehistory.db_client import DBClient
 from pricehistory.receny_util import RecencyUtil
 from pricehistory.source_client import SourceClient
+
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("engineio.server").setLevel(logging.WARNING)
+logging.getLogger("socketio.server").setLevel(logging.WARNING)
+
+LOG = logging.getLogger(__name__)
 
 
 def main():
@@ -20,8 +29,17 @@ def main():
         db_password = config_json["db_password"]
         db_host = config_json["db_host"]
         healthcheck_url = config_json["healthcheck_url"]
+        data_cache_url = config_json["data_cache_url"]
 
-    db_client = DBClient(db_connection_string=DB_CONNECTION_STRING % (db_username, db_password, db_host))
+    if data_cache_url:
+        cache = redis.Redis.from_url(data_cache_url)
+        cache.ping()
+        LOG.info("Using Redis cache for data")
+    else:
+        cache = None
+
+    db_connection_string = DB_CONNECTION_STRING % (db_username, db_password, db_host)
+    db_client = DBClient(db_connection_string=db_connection_string, cache=cache)
 
     cookies = get_cookies(cookie_url)
 
--- pricehistory/db_client.py
+++ pricehistory/db_client.py
@@ -1,26 +1,44 @@
 import dataclasses
+import logging
 from typing import List
 
+import fakeredis
 import pymongo
+import redis
 from pymongo import MongoClient, UpdateOne
 from pymongo.collection import Collection
 from pymongo.operations import InsertOne
 from pymongo.server_api import ServerApi
 
+from .constants import (
+    REDIS_VERSION,
+    PRODUCT_PRICE_HISTORY_CACHE_PREFIX,
+    CATEGORY_PRODUCTS_CACHE_KEY,
+    CATEGORY_NAME_CACHE_KEY,
+)
 from .data.category_document import CategoryDocument
 from .data.price_container import PriceContainer
 
 
+LOG = logging.getLogger(__name__)
+
+
 class DBClient:
-    def __init__(self, db_connection_string: str):
+    def __init__(self, db_connection_string: str, cache: redis.Redis = None):
+        # If no cache is given, spin up a fake one
+        if cache is None:
+            self.cache = fakeredis.FakeStrictRedis(version=REDIS_VERSION)
+        else:
+            self.cache = cache
+
         self.client = MongoClient(db_connection_string, server_api=ServerApi("1"))
 
         # Send a ping to confirm a successful connection
         try:
             self.client.admin.command("ping")
-            print("Successfully connected to database!")
-        except Exception as e:
-            print(e)
+            LOG.info("Successfully connected to database!")
+        except Exception:
+            LOG.exception("Error connecting to database!")
 
         # Collections
         self.database = self.client["price_history"]
@@ -62,12 +80,12 @@ class DBClient:
         self._ensure_category_exists(category_document)
 
         # Ensure we have a document for each product
-        self._ensure_products_exist(price_containers)
+        self._ensure_products_exist(price_containers, category_document)
 
         # Save all the prices to the database
         self._ensure_prices_exist(price_containers)
 
-    def _ensure_products_exist(self, price_containers: List[PriceContainer]):
+    def _ensure_products_exist(self, price_containers: List[PriceContainer], category_document: CategoryDocument):
         operations = []
         for price_container in price_containers:
             product_document = price_container.product_document
@@ -80,40 +98,66 @@ class DBClient:
             )
 
         if not operations:
-            print("Upserted 0 product documents")
+            LOG.info("Upserted 0 product documents")
             return
 
         result = self.products_collection.bulk_write(operations)
-        print(f"Upserted/Modified {result.upserted_count + result.modified_count} product documents")
+        num_documents_changed = result.upserted_count + result.modified_count
+        LOG.info(f"Upserted/Modified {num_documents_changed} product documents")
+
+        if num_documents_changed > 0:
+            # We should reset this category's cache to show the new product data
+            cache_key = f"{CATEGORY_PRODUCTS_CACHE_KEY}_{category_document.id}"
+            self.cache.delete(cache_key)
+            # We should also reset the product display name cache for any products
+            # that actually changed, but this is not easy to do since bulk_write
+            # does not return the ids of modified documents, only the ones that
+            # were upserted (did not exist before). Instead, we just rely on the
+            # one-day cache TTL and know that there may be stale product names
+            # for a single day.
 
     def _ensure_prices_exist(self, price_containers: List[PriceContainer]):
         # Determine which price documents actually need to be saved
         operations = []
         for price_container in price_containers:
             price_document = price_container.price_document
+            product_id = price_document.product_id
 
+            # To save space in the database, we only want to insert documents when the price changes
             most_recent_price_document = self.prices_collection.find_one(
-                filter={"product_id": price_document.product_id}, sort=[("start_date", pymongo.DESCENDING)]
+                filter={"product_id": product_id}, sort=[("start_date", pymongo.DESCENDING)]
             )
-
             if most_recent_price_document:
                 most_recent_price = most_recent_price_document["price_cents"]
                 if most_recent_price == price_document.price_cents:
-                    print(f"Skipping update for product {price_document.product_id} as the price is unchanged")
+                    LOG.info(f"Skipping update for product {product_id} as the price is unchanged")
                     continue
+                else:
+                    LOG.info(f"Updating price for product {product_id}")
+            else:
+                LOG.info(f"New product found: {product_id}")
 
-            print(f"Updating price for product {price_document.product_id}")
             operation = InsertOne(dataclasses.asdict(price_document))
             operations.append(operation)
 
-        if not operations:
-            print("Inserted 0 price documents")
-            return
+        if operations:
+            result = self.prices_collection.bulk_write(operations)
+            LOG.info(f"Inserted {result.inserted_count} price documents")
+        else:
+            LOG.info("Inserted 0 price documents")
 
-        result = self.prices_collection.bulk_write(operations)
-        print(f"Inserted {result.inserted_count} price documents")
+        # We want to wait until the database update happens before we unset the cache entries for any new products or
+        # products that changed prices.
+        for operation in operations:
+            product_id = operation._doc["product_id"]
+            cache_key = f"{PRODUCT_PRICE_HISTORY_CACHE_PREFIX}_{product_id}"
+            self.cache.delete(cache_key)
 
     def _ensure_category_exists(self, category_document: CategoryDocument):
-        self.categories_collection.update_one(
+        update_result = self.categories_collection.update_one(
             filter={"id": category_document.id}, update={"$set": dataclasses.asdict(category_document)}, upsert=True
         )
+
+        if update_result.modified_count > 0:
+            cache_key = f"{CATEGORY_NAME_CACHE_KEY}_{category_document.id}"
+            self.cache.delete(cache_key)
